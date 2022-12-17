396e6f62eeb9a74151f94773804f769847b821f0
hash <hash.vluf@gmail.com>
2022-12-17 18:34:56 +0900

    :chore git test

--- src/sync.ts
+++ src/sync.ts
@@ -1,10 +1,10 @@
-import { sync } from './database';
+// import { sync } from './database';
 
-sync()
-  .then(() => {
-    console.log('data successfully synchronized');
-  })
-  .catch((err) => {
-    console.log(err);
-    console.log('synchronize failed');
-  });
+// sync()
+//   .then(() => {
+//     console.log('data successfully synchronized');
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     console.log('synchronize failed');
+//   });
